664c006f9efa3db40ed6971b4da8166b6e3e8fa7
Ferdinand Engländer <ferdinand@creanimo.net>
2024-05-07 20:10:07 +0000

    basic plugin description header

---
+++ wp-unskippable-notifications-and-approvals.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Unskippable Notifications & Approvals
+* Plugin URI: https://www.creanimo.net
+* Description: Force the confirmation of messages and approvals.
+* Version: 0.1
+* Author: Ferdinand Engländer
+* Author URI: https://www.creanimo.net
+**/
