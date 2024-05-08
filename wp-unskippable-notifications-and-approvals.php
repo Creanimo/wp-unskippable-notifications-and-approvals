4376558edaeb6df60fac9456bec4b431d5cd72af
Ferdinand Engländer <ferdinand@creanimo.net>
2024-05-08 19:58:52 +0000

    enqueue script

--- wp-unskippable-notifications-and-approvals.php
+++ wp-unskippable-notifications-and-approvals.php
@@ -8,6 +8,9 @@
 * Author URI: https://www.creanimo.net
 **/
 
+/**
+ * Set up a menu entry for the settings
+ */
 function unskippable_notifications_settings_page() {
     add_options_page(
         __( 'Unskippable Notifications', 'unskippable-notifications' ),
@@ -20,9 +23,41 @@ function unskippable_notifications_settings_page() {
 
 add_action( 'admin_menu', 'unskippable_notifications_settings_page' );
 
+/**
+ * Load html into settings page
+ */
 function unskippable_notifications_settings_page_html() {
     printf(
         '<div class="wrap" id="unskippable-notifications_settings">%s</div>',
         esc_html__( 'Loading…', 'unskippable-notifications' )
     );
 }
+
+/**
+ * Enqueue scripts for the settings page
+ */
+function unskippable_notifications_settings_page_enqueue_style_script( $admin_page ) {
+    if ( 'settings_page_unskippable-notifications' !== $admin_page ) {
+        return;
+    }
+
+    $asset_file = plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+
+    if ( ! file_exists( $asset_file ) ) {
+        return;
+    }
+
+    $asset = include $asset_file;
+
+    wp_enqueue_script(
+        'unskippable-notifications-script',
+        plugins_url( 'build/index.js', __FILE__ ),
+        $asset['dependencies'],
+        $asset['version'],
+        array(
+            'in_footer' => true,
+        )
+    );
+}
+
+add_action( 'admin_enqueue_scripts', 'unskippable_notifications_settings_page_enqueue_style_script' );
